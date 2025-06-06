eeedfa060036b014eda374bc129d02f0d82fd6d0
Georgi Berchev <g.berchev@gmail.com>
2025-06-06 09:06:31 +0300

    task finished

--- day3_Control_Flow_and_Logical_Operators/treasure_island_task.py
+++ day3_Control_Flow_and_Logical_Operators/treasure_island_task.py
@@ -25,7 +25,28 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 # using backslash as an escape character
-left_right = input('You\'re in a crossroad, where do you want to go? \n Type "left" or "right". \n')
-swim_wait = input('You\'ve come to a lake. There is an island in the middle of the lake? \n Type "wait" to wait for a boat or "swim" to swim across. \n')
-door = input("You arrive at the island unharmed. There is a house with 3 doors.\n One red, one yellow and one blue. Which colour do you choose? \n")
+left_or_right = input('You\'re in a crossroad, where do you want to go? \n Type "left" or "right". \n')
 
+if left_or_right == "left" or left_or_right == "Left":
+    swim_or_wait = input('You\'ve come to a lake. There is an island in the middle of the lake? \n Type "wait" to wait for a boat or "swim" to swim across. \n')
+    
+    if swim_or_wait == "wait" or swim_or_wait == "Wait":
+        door = input("You arrive at the island unharmed. There is a house with 3 doors.\n One red, one yellow and one blue. Which colour do you choose? \n")
+
+        if door == "yellow" or door == "Yellow":
+            print("You win!")
+        elif door == "blue" or "Blue":
+            print("Eaten by beasts. Game Over.")
+        elif door == "red" or door == "Red":
+            print("Burned by fire. Game Over.")
+        else:
+            print("Game Over.")
+    
+    # elif swim_or_wait == "swim" or swim_or_wait == "Swim":
+    #     print("Attacked by trout. Game Over.")
+    else:
+        print("Attacked by trout. Game Over.")
+# elif left_or_right == "right" or left_or_right == "Right":
+#     print("Fell into a hole. Game Over")
+else:
+    print("Fell into a hole. Game Over")
